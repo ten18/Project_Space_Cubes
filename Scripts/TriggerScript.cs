87f2ab0903bf20b8126a3aeb649e83abb8f6f49a
iptcs <37704926+iptcs@users.noreply.github.com>
2020-04-28 01:47:22 -0400

    Add files via upload
    Added a collider to reset the stage when the player falls off

---
+++ Scripts/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+	public SceneManagerScript reloadScript;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		reloadScript.ReloadScene();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
